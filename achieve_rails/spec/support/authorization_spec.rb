a30cb7d87eb76fca743465ab5d53e58b570bf84e
toshinori-m <toshi3587@gmail.com>
2022-08-31 22:30:11 +0900

    sing_inテストの作成

--- achieve_rails/spec/support/authorization_spec.rb
+++ achieve_rails/spec/support/authorization_spec.rb
@@ -1,15 +1,7 @@
 module AuthorizationHelper
   def sign_in(user)
-    # post '/auth/sign_in', params: { email: "guest@example.com", password: "guests" }
-    # post '/auth/sign_in', params: { email: "toshi@gmail.com", password: "toshinori" }
-    # post '/auth/sign_in', params: { email: "toshi@gmail.com", password: "toshinori" }
-    # post '/auth/sign_in', params: { email: user.email, password: user.password }
-    # post '/auth/sign_in', params: { email: user[:email], password: user[:password] },as: :json
-    # post '/auth/sign_in', params: { email: "2@gmail.com", password: "toshinori" }
     post '/auth/sign_in', params: { email: "test1@example.com", password: "password" }
-    # post posts_path, params: { email: user.email, password: user.password }
     
     response.headers.slice('client', 'access-token', 'uid')
-    # response.headers.slice('client', 'uid', 'token-type', 'access-token')
   end
 end
