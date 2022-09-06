8f4c16b07cbda81a4023efc0cccbddb77c1ff763
toshinori-m <toshi3587@gmail.com>
2022-09-06 11:17:13 +0900

    sing_inテスト修正

--- achieve_rails/spec/factories/users.rb
+++ achieve_rails/spec/factories/users.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :user do
-    # name { "example user" }
-    # email { "foo@bar.com" }
-    # password { "foobar123" }
-    # password_confirmation { "foobar123" }
     name { "example" }
     sequence(:email) { |n| "TEST#{n}@example.com" }
     password { "testuser" }
