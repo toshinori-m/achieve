9e10f1b610f3947358bdc15f9a6fdabc70301c36
toshinori-m <toshi3587@gmail.com>
2022-09-11 13:03:37 +0900

    monthly_goalテスト修正（index）

--- achieve_rails/spec/requests/monthly_goals_spec.rb
+++ achieve_rails/spec/requests/monthly_goals_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "MonthlyGoals", type: :request do
-  let!(:user) { User.create(name: 'example', email: 'test1@example.com', password: 'password') }
+  let!(:user) { create(:user) }
+  let!(:monthly_goal) { create(:monthly_goal) }
   let(:token) { sign_in user }
-  let(:json) { JSON.parse(response.body) }
+  let(:params) { { email: user.email, password: user.password } }
 
   describe 'monthly_goal /#create' do
     context 'paramsの値が正しい場合作成成功' do
       it 'MonthlyGoalを作成すると、MonthlyGoalが追加される' do
-        post '/auth/sign_in', params: { email: user.email, password: user.password }, headers: token
+        post '/auth/sign_in', params: params, headers: token
         expect { MonthlyGoal.create(aim: 'test', user_id: user.id) }.to change{MonthlyGoal.count}.by(+1)
       end
     end
 
     context 'paramsの値が正しくない場合作成出来ない' do
       it 'params(aim)の値が空の場合、MonthlyGoalが追加されない' do
-        post '/auth/sign_in', params: { email: user.email, password: user.password }, headers: token
+        post '/auth/sign_in', params: params, headers: token
         expect { MonthlyGoal.create(aim: ' ', user_id: user.id) }.to change{MonthlyGoal.count}.by(0)
       end
     end
@@ -23,38 +24,28 @@ RSpec.describe "MonthlyGoals", type: :request do
 
   describe 'monthly_goal /# index' do
     context 'paramsの値が正しい場合表示成功' do
-      it 'MonthlyGoalの新しい目標を取得(status200 を返す)' do
-        post '/auth/sign_in', params: { email: user.email, password: user.password }, headers: token
-        monthly_goals = MonthlyGoal.where("user_id = '1'").order(id: 'ASC').last(1)
-        monthly_goals_array = monthly_goals.map do |monthly_goal|
-          {
-            id: monthly_goal.id,
-            user_id: monthly_goal.user_id,
-            name: monthly_goal.user.name,
-            aim: monthly_goal.aim,
-            email: monthly_goal.user.email,
-            created_at: monthly_goal.created_at
-          }
-        end
-      expect(response.status).to eq (200)
+      it 'MonthlyGoalの一覧の値を取得(status200 を返す)' do
+        get'/monthly_goals',  headers: token
+        post '/auth/sign_in', params: params, headers: token
+        expect(response.status).to eq (200)
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body.size).to eq 1
+      end
+
+      it 'MonthlyGoalの特定の値を取得(status200 を返す)' do
+        get'/monthly_goals#(monthly_goal.id)',  headers: token
+        post'/auth/sign_in', params: params, headers: token
+        expect(response.status).to eq (200)
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body["data"]["id"]).to eq monthly_goal.id
       end
     end
     
     context 'paramsの値が正しくない場合表示失敗' do
       it 'MonthlyGoalの目標を取得出来ない(status401 を返す)' do
+        get'/monthly_goals'
         post '/auth/sign_in', params: { email: '', password: '' }
-        monthly_goals = MonthlyGoal.where("user_id = '1'").order(id: 'ASC').last(1)
-        monthly_goals_array = monthly_goals.map do |monthly_goal|
-          {
-            id: monthly_goal.id,
-            user_id: monthly_goal.user_id,
-            name: monthly_goal.user.name,
-            aim: monthly_goal.aim,
-            email: monthly_goal.user.email,
-            created_at: monthly_goal.created_at
-          }
-        end
-      expect(response.status).to eq (401)
+        expect(response.status).to eq (401)
       end
     end
   end
