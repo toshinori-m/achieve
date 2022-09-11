7e81c1f583dc91e6e2e8b2bd73a6c8ad3852a6cf
toshinori-m <toshi3587@gmail.com>
2022-09-11 13:16:27 +0900

    postテスト修正

--- achieve_rails/spec/requests/posts_spec.rb
+++ achieve_rails/spec/requests/posts_spec.rb
@@ -2,29 +2,30 @@ require 'rails_helper'
 RSpec.describe 'POST /create' , type: :request do
   let(:user) { User.create(name: 'example', email: 'test1@example.com', password: 'password')  }
   let(:token) { sign_in user }
+  let(:params) { { email: user.email, password: user.password } }
 
   context 'ログイン成功' do
     it 'status200が返ってくる' do
-      post '/auth/sign_in', params: { email: user.email, password: user.password }, headers: token
+      post '/auth/sign_in', params: params, headers: token
       expect(response.status).to eq (200)
     end
   end
 
   context 'ログイン失敗' do
     it '誤ったemailを入力したstatus401が返ってくる' do
-      post '/auth/sign_in', params: { email: 'toshi@nori.com', password: user.password }, headers: token
+      post '/auth/sign_in', params: { email: 'test@test.com', password: user.password }, headers: token
       expect(response.status).to eq (401)
     end
 
     it '誤ったpasswordを入力したstatus401が返ってくる' do
-      post '/auth/sign_in', params: { email: user.email, password: 'toshinori' }, headers: token
+      post '/auth/sign_in', params: { email: user.email, password: 'test1' }, headers: token
       expect(response.status).to eq (401)
     end
   end
 
   context 'ログアウト成功' do
     it 'status200が返ってくる' do
-      delete '/auth', params: { email: user.email, password: user.password }, headers: token
+      delete '/auth', params: params, headers: token
       expect(response.status).to eq (200)
     end
   end
@@ -43,7 +44,7 @@ RSpec.describe 'POST /create' , type: :request do
     it 'status401を返す' do
       for i in routes do
         if route_paths === i
-          post '/auth/sign_in', params: { email: user.email, password: user.password }, headers: token 
+          post '/auth/sign_in', params: params, headers: token 
           expect(response.status).to eq(200)
         else
           post '/auth/sign_in', params: { email: '', password: '' }
