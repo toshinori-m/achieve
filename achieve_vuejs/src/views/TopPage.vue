7a0628bfd1c4f00ae6e37e6e86a1f0bf8115ca95
toshinori-m <toshi3587@gmail.com>
2022-12-12 17:21:32 +0900

    toppage導入

---
+++ achieve_vuejs/src/views/TopPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-[url('./img/achieve.png')"></div>
+</template>
+
+<script>
+// import LoginForm from '../components/welcom/LoginForm.vue'
+// import SignupForm from '../components/welcom/SignupForm.vue'
+
+// export default {
+//   components: { LoginForm, SignupForm },
+//   data () {
+//     return {
+//       shouldShowLoginForm: false
+//     }
+//   },
+//   methods: {
+//     redirectToHome () {
+//       this.$router.push({ name: 'HomePage' })
+//     }
+//   } 
+// }
+</script>
+
+<style>
+</style>
--- achieve_vuejs/tailwind.config.js
+++ achieve_vuejs/tailwind.config.js
@@ -1,7 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
