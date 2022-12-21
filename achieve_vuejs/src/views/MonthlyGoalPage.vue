9c274e4bbe649dc72ad8369332a2ced6462fadce
toshinori-m <toshi3587@gmail.com>
2022-12-21 12:26:02 +0900

    MonthlyGoalPage tailwindcss導入

--- achieve_vuejs/src/views/MonthlyGoalPage.vue
+++ achieve_vuejs/src/views/MonthlyGoalPage.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="monthly_goal">
-    <h2>今月目標</h2>
-    <form @submit.prevent ="monthly_goal">
-      <input type="aim" required placeholder="今月目標を入力" v-model="aim">
+  <div class="py-36 text-center">
+    <h2 class="text-3xl tracking-widest mb-7 text-center text-white text-shadow-b">今月目標</h2>
+    <form class="sm:w-4/5 md:w-3/4 my-14 mx-auto" @submit.prevent ="monthly_goal">
+      <input class="rounded-2xl w-full py-2 px-3" type="aim" required placeholder="今月目標を入力" v-model="aim">
       <div class="error">{{ error }}</div>
-      <button class="ok_button">OK</button>
+      <button class="mt-10 ok_button">OK</button>
     </form>
   </div>
 </template>
@@ -46,32 +46,4 @@ export default {
 </script>
 
 <style>
-.monthly_goal {
-  text-align: center;
-  padding: 150px 0;
-}
-.monthly_goal h2{
-  font-size: 30px;
-  letter-spacing: 5px;
-  margin-bottom: 30px;
-  text-align: center;
-  color: white;
-  text-shadow: 1px 1px 2px blue;
-}
-.monthly_goal form {
-  width: 800px;
-  margin: 60px auto;
-}
-.monthly_goal input {
-  width: 100%;
-  padding: 8px 20px;
-  margin: 8px auto;
-  border-radius: 4px;
-  border: 1px solid rgb(238, 238, 238);
-  outline: none;
-  box-sizing: border-box;
-}
-.monthly_goal .ok_button {
-  margin: 30px auto;
-}
 </style>
