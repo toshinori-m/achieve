7eb77282e50c6883bbdc64f14f0360c69214efab
toshinori-m <toshi3587@gmail.com>
2022-12-21 12:42:55 +0900

    message_send エラー修正

--- achieve_rails/app/channels/room_channel.rb
+++ achieve_rails/app/channels/room_channel.rb
@@ -9,8 +9,8 @@ class RoomChannel < ApplicationCable::Channel
 
   def receive(data)
     user = User.find_by(email: data['email'])
-
-    if message == Message.create(content: data['message'], user_id: user.id)
+    
+    if message = Message.create(content: data['message'], user_id: user.id)
       ActionCable.server.broadcast 'room_channel',
                                    { message: data['message'], name: user.name, created_at: message.created_at }
     end
