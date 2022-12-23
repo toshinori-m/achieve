033dbd220aa424bfca73fb39114db98f8e762008
toshinori-m <toshi3587@gmail.com>
2022-12-23 09:03:34 +0900

    ChatWindow tailwindcss追加

--- achieve_vuejs/src/components/chatroom/ChatWindow.vue
+++ achieve_vuejs/src/components/chatroom/ChatWindow.vue
@@ -3,7 +3,7 @@
     <div v-if="messages" class="max-h-96 overflow-auto text-xs sm:text-sm" ref="messages">
       <ul class="list-none" v-for="message in messages" :key="message.id">
         <li class="inline-block clear-both" :class="{ received: message.email !== uid, sent: message.email == uid }">
-          <span class="relative mr-1.5 block text-xs">{{ message.name }}</span>
+          <span class="mr-1.5 block text-xs">{{ message.name }}</span>
           <div class="message relative" @dblclick="handleLike(message)">
             {{ message.content }}
             <div v-if="message.likes.length" class="absolute bg-white flex justify-around items-center rounded-l-full w-7 z-30 border-2 border-gray-300 p-px z-20 -bottom-2.5 right-0 text-xs">
@@ -90,84 +90,4 @@ export default {
 </script>
 
 <style scoped>
-  .chat-window {
-    /* background: white; */
-    /* padding: 30px 20px; */
-    /* border-bottom: 1px solid #eee; */
-  }
-  ul {
-    /* list-style: none; */
-    /* margin: 0;
-    padding: 0; */
-  }
-  ul li {
-    /* display:inline-block; */
-    /* clear: both; */
-  }
-  .received .message {
-    background: #eee;
-    padding: 10px;
-    display: inline-block;
-    border-radius: 30px;
-    margin-bottom: 2px;
-    max-width: 400px;
-  }
-  .received {
-    float: left;
-  }
-  .sent {
-    float: right;
-  }
-  .sent .message {
-    background: #677bb4;
-    padding: 10px;
-    display: inline-block;
-    border-radius: 30px;
-    margin-bottom: 2px;
-    max-width: 400px;
-  }
-  .name {
-    /* position: relative; */
-    /* margin-right: 6px;
-    display: block;
-    font-size: 13px; */
-  }
-  .created-at {
-    /* display: block; */
-    /* color: #999; */
-    /* font-size: 12px; */
-    /* margin-bottom: 20px; */
-    /* margin-left: 4px; */
-    /* margin-top: 3px; */
-  }
-  .messages {
-    /* max-height: 400px; */
-    /* overflow: auto; */
-  }
-  .message {
-    /* position: relative; */
-  }
-  .heart-container {
-    /* background: white; */
-    /* position: absolute; */
-    /* display: flex; */
-    /* justify-content: space-around; */
-    /* align-items: center; */
-    /* border-radius: 30px; */
-    /* min-width: 25px; */
-    /* border-style: solid; */
-    /* border-width: 1px; */
-    /* border-color: rgb(245, 245, 245); */
-    /* padding: 1px 2px; */
-    /* z-index: 2; */
-    /* bottom: -7px; */
-    /* right: 0px; */
-    /* font-size: 9px; */
-  }
-  .received .message::selection {
-    /* background: #eee; */
-  }
-  .sent .message::selection {
-    /* background: #677bb4; */
-  }
 </style>
--- achieve_vuejs/tailwind.config.js
+++ achieve_vuejs/tailwind.config.js
@@ -37,11 +37,31 @@ module.exports = {
         ".text-shadow-2xl": {
           textShadow: "0px 10px 3px lime"
         },
-        ".text-shadow-none": {
-          textShadow: "none"
+
+        // ChatWindow.vue
+        ".received .message": {
+          background: "#eee",
+          padding: "10px",
+          display: "inline-block",
+          borderRadius: "30px",
+          marginBottom: "2px",
+          maxWidth: "400px"
+        },
+        ".received": {
+          float: "left"
+        },
+        ".sent": {
+          float: "right"
+        },
+        ".sent .message": {
+          background: "#677bb4",
+          padding: "10px",
+          display: "inline-block",
+          borderRadius: "30px",
+          marginBottom: "2px",
+          maxWidth: "400px"
         }
       };
-
       addUtilities(newUtilities);
     }],
 }
