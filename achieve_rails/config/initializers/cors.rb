f8bc52cc1e18d0613f68c8f17bf4991d82e934ce
toshinori-m <toshi3587@gmail.com>
2025-06-21 17:24:24 +0900

    achieve_rails/config/initializers/cors.rb無効化

--- achieve_rails/config/initializers/cors.rb
+++ achieve_rails/config/initializers/cors.rb
@@ -5,13 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins '*'
 
-    resource '*',
-             headers: :any,
-             expose: %w[access-token expiry token-type uid client],
-             methods: %i[get post put patch delete options head]
-  end
-end
+#     resource '*',
+#              headers: :any,
+#              expose: %w[access-token expiry token-type uid client],
+#              methods: %i[get post put patch delete options head]
+#   end
+# end
