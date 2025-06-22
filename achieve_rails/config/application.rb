e3e291e2443b00867204dfd328e469e3156be841
toshinori-m <toshi3587@gmail.com>
2025-06-22 15:22:33 +0900

    auth確認

--- achieve_rails/config/application.rb
+++ achieve_rails/config/application.rb
@@ -36,14 +36,5 @@ module AchieveRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'https://achieve-git-setup-render-deploy-toshinori-ms-projects.vercel.app'
-        resource '*',
-                 headers: :any,
-                 expose: %w[access-token expiry token-type uid client],
-                 methods: %i[get post put patch delete options head]
-      end
-    end
   end
 end
--- achieve_rails/config/initializers/cors.rb
+++ achieve_rails/config/initializers/cors.rb
@@ -8,11 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    # origins [
-    #   ENV['BRANCH_ORIGIN'],
-    #   ENV['FRONTEND_ORIGIN'],
-    #   'http://localhost:80'
-    # ].compact
 
     resource '*',
              headers: :any,
